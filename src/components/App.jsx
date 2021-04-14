cfb280a63bc894e801a21b74370f985df304087c
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-14 22:17:53 +0200

    contacts printed with map method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Card from "../components/Card";
+import contacts from "../contacts";
 
 function App() {
   return (
-    
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map((contact) => (
+        <Card
+          name={contact.name}
+          imgURL={contact.imgURL}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
+    </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,17 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">props.name</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-            className="circle-img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img src={props.imgURL} alt="avatar_img" className="circle-img" />
+      </div>
+      <div className="bottom">
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
       </div>
     </div>
   );
 }
+
+export default Card;
