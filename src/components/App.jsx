f1891540b64febde53915b6e1ea64f63cdde9228
William Truppi <72492620+williamtruppi@users.noreply.github.com>
2021-04-14 21:54:23 +0200

    Created card.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-            className="circle-img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
-    </div>
+    
   );
 }
 
